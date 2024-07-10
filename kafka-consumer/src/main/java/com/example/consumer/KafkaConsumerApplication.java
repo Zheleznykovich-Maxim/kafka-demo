0aa7c44cabc223ea00f6c1beb649aa932733585f
Maxim Zheleznykovich <maxssmail572@gmail.com>
2024-07-11 00:02:16 +0300

    Initial commit

---
+++ kafka-consumer/src/main/java/com/example/consumer/KafkaConsumerApplication.java
@@ -0,0 +1,11 @@
+package com.example.consumer;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaConsumerApplication {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ kafka-producer/src/main/java/com/example/producer/KafkaProducerApplication.java
@@ -0,0 +1,10 @@
+package com.example.producer;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaProducerApplication {
+    public static void main(String[] args) {
+
+    }
+}
